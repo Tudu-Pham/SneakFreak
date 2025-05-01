22d08f8e2871c1299cd8322070fe96d38c71f9fd
Pham Trung Dung <dungphamtrung002@gmail.com>
2025-05-01 20:03:18 +0700

    mvc

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import 'dotenv/config';
+import webRoutes from "./routes/web";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,9 +9,8 @@ const port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
-    res.render("home")
-})
+//config routes
+webRoutes(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
