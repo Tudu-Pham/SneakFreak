9942573181ebb158ac0b667fbc1f13bb8b048b44
Pham Trung Dung <dungphamtrung002@gmail.com>
2025-05-01 19:06:47 +0700

    home page

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-//const express = require('express')
-import express from "express"
-import 'dotenv/config'
-const app = express()
-const port = process.env.PORT || 8080
+import express from "express";
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    res.send('Hello World Tudu!')
+    res.render("home")
 })
 
 app.listen(port, () => {
